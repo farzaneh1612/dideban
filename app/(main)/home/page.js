dbef19c776ff613bf330b26c727522c8d83c7780
farzaneh1612 <nalchi.f@gmail.com>
2024-06-10 07:38:36 +0330

    fix firtst page

--- app/(main)/home/page.js
+++ app/(main)/home/page.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>به هیرکانیکو خوش آمدیدلطفا صفحه مورد نظر خود را از  منو انتخاب کنید </div>
   )
 }
